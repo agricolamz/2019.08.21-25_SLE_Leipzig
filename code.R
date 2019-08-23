c2488e7ab039d5639f5a26125088816bbcc32f44
George Moroz <agricolamz@gmail.com>
2019-08-23 21:46:50 +0300

    add all

--- code.R
+++ code.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(extrafont)
-theme_set(theme_minimal(base_family = "Roboto Condensed")+
+theme_set(theme_bw(base_family = "Roboto Condensed")+
             theme(text = element_text(size = 19, family = "Brill")))
 library(waffle)
 library(qrcode)
@@ -21,25 +21,24 @@ lingtypology::glottolog.modified %>%
   all_langs
 
 
-  count(affiliation, area, sort = TRUE) %>% 
-  mutate(rank = 1:n()) ->
+set.seed(42)
+all_langs %>% 
+  mutate(value =  sample(c("value 1", "value 2"),
+                           7420, 
+                           replace = TRUE,
+                           prob = c(0.25, 0.75)),
+         group = "total = 7420\nvalue 1 = 1891\nθ = 0.254") ->
   all_langs
 
-set.seed(42)
-
-all_langs <- tibble(group = "total = 7420\nvalue 1 = 1891\nθ = 0.254",
-                    value = sample(c("value 1", "value 2"),
-                                   7420,
-                                   replace = TRUE,
-                                   prob = c(0.25, 0.75)))
 
 all_langs %>% 
-  count(group, value)
+  count(value)
 
+set.seed(42)
 all_langs %>%
   sample_n(200) %>%
 #  count(group, value)
-  mutate(group = "total = 200\nvalue 1 = 62\nθ = 0.235") %>%
+  mutate(group = "total = 200\nvalue 1 = 53\nθ = 0.265") %>%
   rbind(all_langs) %>%
   count(group, value) ->
   simple_sample_results
@@ -71,9 +70,89 @@ all_langs %>%
   labs(y = "logarithm number of languages")+
   theme(legend.position = "top")
 
-ggsave(filename = "05_families_by_area.jpeg", 
+ggsave(filename = "04_families_by_area.jpeg", 
        width = 200, 
        height = 150, 
        units = "mm",
        device = "jpeg")
 
+all_langs %>% 
+  count(area, affiliation) %>% 
+  ggplot(aes(fill = affiliation, values = n)) +
+  geom_waffle(color = "white", size = .3, n_rows = 70, flip = TRUE, show.legend = FALSE) +
+  facet_wrap(~area, ncol = 1, scales = "free_y")+
+  scale_x_discrete()+
+  scale_y_continuous(labels = function(x) x * 70, # make this multiplyer the same as n_rows
+                     expand = c(0,0))+
+  theme(panel.grid = element_blank())
+
+ggsave(filename = "05_families_by_area.jpeg", 
+       width = 100, 
+       height = 150, 
+       units = "mm",
+       device = "jpeg")
+
+
+library(lingtypology)
+read_csv("https://github.com/phoible/dev/raw/master/data/phoible.csv") %>% 
+  filter(Source == "upsid") %>% 
+  select(Glottocode, LanguageName, Phoneme) %>% 
+  mutate(affiliation = aff.lang(lang.gltc(Glottocode)),
+         affiliation = ifelse(str_detect(affiliation, ","), str_extract(affiliation, ".*?,"), affiliation),
+         affiliation = str_remove(affiliation, ",")) %>% 
+  filter(!is.na(affiliation)) %>% 
+  write_csv("data/phoible_upsid.csv")
+
+upsid <- read_csv("data/phoible_upsid.csv")
+
+set.seed(11)
+upsid %>% 
+  mutate(vowel = str_extract(Phoneme, "a|u|i")) %>% 
+  count(Glottocode, affiliation, LanguageName, vowel) %>% 
+  filter(!is.na(vowel)) %>% 
+  mutate(n = 1,
+         area = area.lang(lang.gltc(Glottocode))) %>% 
+  filter(!is.na(area)) %>% 
+  spread(vowel, n, fill = 0) %>% 
+  gather(vowel, value, a:u) %>% 
+  group_by(area, affiliation, vowel) %>% 
+  summarise(n = n(),
+            ratio = sum(value)/n) %>% 
+  spread(vowel, ratio) %>% 
+  ungroup() %>% 
+  filter(n > 2) %>% 
+  sample_n(10) %>% 
+  gather(vowel, ratio, a:u) %>% 
+  mutate(affiliation = paste0(affiliation, " (", n, ")"),
+         caption = paste("sample of ", sum(n)/3, " languages")) ->
+for_plot  
+for_plot  %>% 
+  ggplot(aes(n, ratio, label = affiliation, color = area))+
+  ggrepel::geom_text_repel(show.legend = FALSE, size = 4)+
+  geom_point(aes(size = n), show.legend = FALSE)+
+  facet_wrap(~vowel)+
+  labs(x = "number of languages",
+       y = "ratio of languages with a sound",
+       caption = unique(for_plot$caption))
+
+ggsave(filename = "images/06_families_sample.jpeg", 
+       width = 200, 
+       height = 150, 
+       units = "mm",
+       device = "jpeg")
+
+for_plot  %>% 
+  ggplot(aes(ratio)) +
+  geom_density()+
+  facet_wrap(~vowel)+
+  labs(caption = unique(for_plot$caption))+
+  xlim(0, 1)
+
+ggsave(filename = "images/07_distributions.jpeg", 
+       width = 200, 
+       height = 150, 
+       units = "mm",
+       device = "jpeg")
+
+  
+  
